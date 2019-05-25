5c26a53fb37aa04a50205465523cdcdd8e139917
Taylor Thurlow <taylorthurlow8@gmail.com>
2019-05-25 12:14:00 -0700

    Run rails app:update to 5.2

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
 require "action_view/railtie"
@@ -19,33 +20,11 @@ Bundler.require(*Rails.groups)
 module RailsBamboo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Use the responders controller from the responders gem
-    config.app_generators.scaffold_controller :responders_controller
-
-    # Add bower components to asset path
-    # config.assets.paths << "#{Rails.root}/app/assets/bower_components"
-
-    # Autoload files in lib directory
-    config.eager_load_paths << "#{Rails.root}/lib"
-
-    # Configure default Rails generators
-    config.generators do |g|
-      # Don't generate scaffold.scss stylesheet
-      g.scaffold_stylesheet false
-
-      # Don't generate scaffold stylesheets, helpers, or javascripts
-      g.assets false
-      g.helper false
-
-      # Use a custom scaffold template engine so we can change things
-      g.template_engine :all
-      g.fallbacks[:all] = :erb
-    end
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    "Cache-Control" => "public, max-age=#{1.hour.seconds.to_i}",
+    "Cache-Control" => "public, max-age=#{1.hour.to_i}",
   }
 
   # Show full error reports and disable caching.
@@ -28,15 +28,12 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  # Store uploaded files on the local file system in a temporary directory
+  config.active_storage.service = :test
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # Configure bullet
-  config.after_initialize do
-    Bullet.enable = true                  # enable bullet
-    Bullet.raise = true                   # raise errors when failing
-  end
 end
